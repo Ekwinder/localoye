49eb834513077d008b5eb6136c4635edf6448c45
Ekwinder <ekwindersaini@gmail.com>
2015-07-17 09:17:19 +0530

    another one

--- localoyeproject/settings.py
+++ localoyeproject/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #'questions',
+    'questions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,12 +100,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-DEBUG = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+    
+DEBUG = False
 
 try:
     from .local_settings import *
 
 except ImportError:
     pass
+
+LOGIN_REDIRECT_URL  =   '/'
+
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'PAGE_SIZE': 10
+}
