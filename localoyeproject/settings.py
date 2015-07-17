2f407c8e5dc99e7cddec972c413c74283bc005c1
Ekwinder <ekwindersaini@gmail.com>
2015-07-17 09:19:12 +0530

    settings

--- localoyeproject/settings.py
+++ localoyeproject/settings.py
@@ -100,6 +100,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
     
